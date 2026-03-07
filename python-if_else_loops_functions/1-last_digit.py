60934cffd5c35e14fc40e9e45e25bf0a4ecc3e0e
Azer382 <mammedof.azer>
2026-03-07 11:38:03 +0000

    Fix pycodestyle trailing whitespace

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ if number >= 0:
     last_digit = number % 10
 else:
     last_digit = number % -10
-output = f"Last digit of {number} is {last_digit}" 
+output = f"Last digit of {number} is {last_digit}"
 if last_digit > 5:
     print(f"{output} and is greater than 5")
 elif last_digit == 0:
