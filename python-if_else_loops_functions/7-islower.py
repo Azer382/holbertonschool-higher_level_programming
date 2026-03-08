ffe457266516c4176da7c1b3ee42d7768e736f9d
Azer382 <mammedof.azer@gmail.com>
2026-03-08 17:07:46 +0000

    First commit

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
 def islower(c):
-    """Simvolun kiçik hərf olub-olmadığını yoxlayır."""
-    # ord(c) funksiyası simvolun ASCII rəqəmini verir
     if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
