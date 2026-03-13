11e5f8379193bc180afc406ef589d0f5c0ab726a
Azer382 <mammedof.azer@gmail.com>
2026-03-13 09:31:34 +0000

    update 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 import sys
 
 if __name__ == "__main__":
-    n = len(sys.argv) -1
+    n = len(sys.argv) - 1
 
     if n == 0:
         print("0 arguments.")
@@ -13,4 +13,3 @@ if __name__ == "__main__":
 
     for i in range(1, n + 1):
         print("{}: {}".format(i, sys.argv[i]))
-
