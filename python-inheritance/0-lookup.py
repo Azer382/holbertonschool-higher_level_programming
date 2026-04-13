6eff839b6dcd5c3f59a104c0248e40928ddc84a2
Azer382 <mammedof.azer@gmail.com>
2026-04-13 14:00:37 +0000

    First commit

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module contains the function of lookup."""
+
+
+def lookup(obj):
+    """Returns a list of available attributes and methods an object."""
+    return dir(obj)
