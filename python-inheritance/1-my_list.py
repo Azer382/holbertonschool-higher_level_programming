7a3ac5d4887ed247fcaeb91348befa7ad2519eab
Azer382 <mammedof.azer@gmail.com>
2026-04-20 13:37:44 +0000

    added test file

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class MyList that inherits from list"""
+
+class MyList(list):
+    """prints the list sorted"""
+
+    def print_sorted(self):
+        """prints the list sorted"""
+        new_list = self[:]
+        new_list.sort()
+        print("{}".format(new_list))
